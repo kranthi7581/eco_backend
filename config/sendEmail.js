05270ca670d849984a9fb47c8fbd298d337706f9
kranthi7581 <kranthikumarakula7@gmail.com>
2026-06-03 15:16:10 +0530

    user modif

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -4,10 +4,16 @@ require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const sendEmail = async (email, subject, message, html) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // false for 587 (STARTTLS), true for 465
     auth: {
       user: process.env.MY_EMAIL,
       pass: process.env.MY_PASSWORD,
+    },
+    family: 4, // Force IPv4 to prevent ENETUNREACH errors on cloud platforms like Render
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
